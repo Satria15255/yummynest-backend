71c7a0badce43c8bdcfb2007760db7d540cf24b4
satriazafarani <satriazafarani05@gmail.com>
2025-06-16 08:14:55 +0700

    fix: update CORS config

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+const allowedOrigins = ["http://localhost:3000", "https://yummynest-foodrecipe.vercel.app"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
