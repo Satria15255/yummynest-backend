332cec5059b50956db4e0a57eca70e28ee63a746
satriazafarani <satriazafarani05@gmail.com>
2025-06-18 10:25:38 +0700

    update fix cloudinary add dotenv

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use(
 );
 
 app.use(express.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/recipes", recipeRoutes);
